f5c1c50c80bd173cb60bad75da2e5e65bf299bc0
Fahath20 <fahathr@gmail.com>
2020-07-13 17:41:26 -0500

    Initial Commit

---
+++ Geofencing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Geofencing
+//
+//  Created by Fahath Rajak on 13/07/20.
+//  Copyright © 2020 Fahath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
